7550137f05f337d9edfe57816e3e175feec1ced8
Chatanga <spica@Virgo.(none)>
2014-08-17 13:35:20 +0200

    Add a dist target to the build process.

---
+++ native/girinoscope.bat
@@ -0,0 +1,9 @@
+@echo off
+set SCRIPT_PATH=%~dp0
+set SCRIPT_DIR=%SCRIPT_PATH:~0,-1%
+
+set PATH=C:\Program Files (x86)\Arduino\java\bin;%JAVA_HOME%\bin;%PATH%
+start javaw -cp "%SCRIPT_DIR%\lib\Girinoscope.jar;%SCRIPT_DIR%\lib\RXTXcomm.jar" ^
+-Dgirino.native.path=native ^
+-Dgirino.native.path="%SCRIPT_DIR%" ^
+org.hihan.girinoscope.ui.UI
