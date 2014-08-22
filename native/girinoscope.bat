f68afd3931704320fc01e6fccc35c29f18d6def7
Chatanga <spica@Virgo.(none)>
2014-08-22 20:36:05 +0200

    Correct launch script on Mac.

--- native/girinoscope.bat
+++ native/girinoscope.bat
@@ -3,6 +3,6 @@ set SCRIPT_PATH=%~dp0
 set SCRIPT_DIR=%SCRIPT_PATH:~0,-1%
 
 set PATH=C:\Program Files (x86)\Arduino\java\bin;%JAVA_HOME%\bin;%PATH%
-start javaw -cp "%SCRIPT_DIR%\lib\Girinoscope.jar;%SCRIPT_DIR%\lib\RXTXcomm.jar" ^
+java -cp "%SCRIPT_DIR%\lib\Girinoscope.jar;%SCRIPT_DIR%\lib\RXTXcomm.jar" ^
 -Dgirinoscope.native.path="%SCRIPT_DIR%" ^
 org.hihan.girinoscope.ui.UI
